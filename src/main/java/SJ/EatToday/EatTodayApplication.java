910a8be557273e6b80b2e7b88a37f1afe3459342
sehwan24 <1224ssanta@naver.com>
2023-05-05 17:28:56 +0900

    first commit

---
+++ src/main/java/SJ/EatToday/EatTodayApplication.java
@@ -0,0 +1,13 @@
+package SJ.EatToday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatTodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SJ/EatToday/EatTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package SJ.EatToday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
