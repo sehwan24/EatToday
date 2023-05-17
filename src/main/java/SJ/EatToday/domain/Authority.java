6a68a6a1d40b66abd0191828c3166f1d311ea760
sehwan24 <1224ssanta@naver.com>
2023-05-17 19:25:06 +0900

    commit

---
+++ src/main/java/SJ/EatToday/domain/Authority.java
@@ -0,0 +1,5 @@
+package SJ.EatToday.domain;
+
+public enum Authority {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/SJ/EatToday/domain/Member.java
@@ -0,0 +1,38 @@
+package SJ.EatToday.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor  //기본 생성자 생성
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Builder
+    public Member(String username, String password, String email, Authority authority) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.authority = authority;
+    }
+}
