71147ca8a65f4321fa1af8aa8dbf205a823e49ca
sehwan24 <1224ssanta@naver.com>
2023-06-11 18:09:50 +0900

    !

--- src/main/java/SJ/EatToday/controller/RestaurantController.java
+++ src/main/java/SJ/EatToday/controller/RestaurantController.java
@@ -3,14 +3,15 @@ package SJ.EatToday.controller;
 import SJ.EatToday.domain.Restaurant;
 import SJ.EatToday.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/restaurant")
+
+@Controller
 @RequiredArgsConstructor
 public class RestaurantController {
 
